1f7ae5c58f2aefd4a7de0795af5b1407736b63fc
sohel5G <contactsohelrana>
2023-08-16 13:50:07 +0600

    first commit

---
+++ js/event.js
@@ -0,0 +1,17 @@
+
+
+function yellow(){
+    document.body.style.backgroundColor='yellow';
+}
+
+document.getElementById('blue').onclick = function(){
+    document.body.style.backgroundColor='blue';
+}
+
+document.getElementById('Purple').addEventListener('click', function purple(){
+    document.body.style.backgroundColor='purple';
+})
+
+
+
+
