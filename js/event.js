5c09aed1a464817ff6d417be9f9d35ebaa458e2e
sohel5G <contactsohelrana>
2023-08-16 17:41:23 +0600

    Git delete button practise done

--- js/event.js
+++ js/event.js
@@ -1,5 +1,5 @@
 
-
+/*
 function yellow(){
     document.body.style.backgroundColor='yellow';
 }
@@ -11,6 +11,59 @@ document.getElementById('blue').onclick = function(){
 document.getElementById('Purple').addEventListener('click', function purple(){
     document.body.style.backgroundColor='purple';
 })
+
+*/
+
+
+// document.getElementById('text-for-add').addEventListener('click', function(){
+//     document.getElementById('text-added').innerText = 'This is added after click on the button';
+// });
+
+
+
+// document.getElementById('upbtn').addEventListener('click', function(){
+//     document.getElementById('updated-text').innerText = document.getElementById('input').value
+//     document.getElementById('input').value = '';
+// });
+
+
+/*
+document.getElementById('btn-submit').addEventListener('click', function () {
+    let getTestAreaValue = document.getElementById('textarea').value;
+    let p = document.createElement('p');
+    p.innerText = getTestAreaValue;
+    document.getElementById('comments-box').appendChild(p);
+    document.getElementById('textarea').value = '';
+});
+*/
+
+
+
+
+/*
+let deleteBtn = document.getElementById('btn-delete');
+let input = document.getElementById('input');
+
+// input.addEventListener('keyup', function(){
+//     if(input.value === 'delete'){
+//         deleteBtn.removeAttribute('disabled');
+//     }else{
+//         deleteBtn.setAttribute('disabled', '');
+//     }
+// });
+
+input.addEventListener('keyup', function(getEvent){
+    if(getEvent.target.value === 'delete'){
+        deleteBtn.removeAttribute('disabled');
+    }else{
+        deleteBtn.setAttribute('disabled', true);
+    }
+});
+
+deleteBtn.addEventListener( 'click', function(){
+    document.getElementById('secret-info').style.visibility='hidden';
+} );
+*/
 
 
 
